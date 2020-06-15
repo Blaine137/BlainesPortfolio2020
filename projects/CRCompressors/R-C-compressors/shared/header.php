a9198fa132f56f56a305cf9cc29d3d4db7d45e91
Blaine137 <52352305+Blaine137@users.noreply.github.com>
2020-06-15 10:42:19 -0400

    added the files for old portfolio
    going to start making the content better

---
+++ projects/CRCompressors/R-C-compressors/shared/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Font Awsome -->
+	<link rel="stylesheet preconnect" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<!-- set browser color to them color. only supported in sertain mobile devices as of 5/13/19-->
+	<meta name="theme-color" content="#003478">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#003478">
+
+	<link rel="stylesheet preload" type="text/css" href="https://blaineyoung.tech/projects/CRCompressors/R-C-compressors/css/styles.css">
+	<link rel="stylesheet preload" href="https://blaineyoung.tech/projects/CRCompressors/R-C-compressors/css/animate.css">
+	<!-- ADD DESCRIPTION SEO OPT JASON LD-->
+	<meta name="description" content="Free evaluation for first time customers and 24/7 Emergency Repair. C&R Compressors can handle all your air compressors needs in Georgia!" />
+	<link rel="icon" type="image/jpg" href="images/r&c_logo.jpg">
+	<title>C&amp;R Compressors</title>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php#hero_id"><img src="images/r&c_logo.jpg" alt="C&R Compressors logo" class="logo"></a></li>
+				<li><a href="index.php#services_id">Services</a></li>
+				<li><a href="index.php#area_id">Area</a></li>
+				<li><a href="index.php#time_id">Time</a></li>
+				<li><a href="index.php#about_id">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+            <div class="mobile_nav_logo">
+				<div class="line"></div>
+				<div class="line"></div>
+				<div class="line"></div>
+			</div>
+		</nav>
+		<div class="mobile_nav">
+			<ul>
+				<li><a href="index.php#hero_id"><img src="images/r&c_logo.jpg" alt="C&R compressors logo" class="logo"></a></li>
+				<li><a href="index.php#services_id" >Services</a></li>
+				<li><a href="index.php#area_id" >Area</a></li>
+				<li><a href="index.php#time_id" >Time</a></li>
+				<li><a href="index.php#about_id">About</a></li>
+				<li><a href="contact.php" >Contact</a></li>
+
+			</ul>
+		</div>
+	</header>
+<main>
